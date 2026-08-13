43e6fdf49035c284997e58ee25f21a75a11ef499
BenisMulenda <benismulenda54@gmail.com>
2026-08-13 13:48:26 +0200

    Initialisation

--- Frontend-new/.next/dev/server/server-reference-manifest.js
+++ Frontend-new/.next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aH20juULlbndCPfnKPM7wS1s3Siav6/6J8TX61914fE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
